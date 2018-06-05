e094c29451214da7e516c3235ab9cab170fef3f0
sp6men <37633117+sp6men@users.noreply.github.com>
2018-06-05 09:51:58 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+var titre = document.querySelector("h3");
+var p = document.querySelector("p");
+var btn1 = document.querySelector(".btn1");
+var btn2 = document.querySelector(".btn2");
+var btn3 = document.querySelector(".btn3");
+var btn4 = document.querySelector(".btn4");
+var btn5 = document.querySelector(".btn5");
+var btn6 = document.querySelector(".btn6");
+
+
+btn1.addEventListener("click", function() {
+
+	p.textContent = "l'épaule :                        est la région morphologique se situant à la jonction du tronc avec le membre supérieur.Elle comporte plusieurs articulations qui concourent à en faire le complexe articulaire le plus mobile du corps humain. Elle permet d'orienter le membre supérieur dans l'espace, autorisant en particulier son extrémité effectrice, la main, à assurer ses rôles de préhension et de communication avec l'environnement situé à sa portée.";
+});
+
+btn2.addEventListener("click", function() {
+	p.textContent = "Le poignet : région du membre supérieur située entre la main et l'avant-bras, et contenant le carpe. Élément-clé pour le fonctionnement de la main, il permet les mouvements (c'est-à-dire les changements de place et d’orientation) de la main par rapport à l’avant-bras, transmet les forces appliquées de la main à l’avant-bras, permet d'adapter la capacité de flexion-extension maximale des doigts et de la préhension.";
+});
+
+btn3.addEventListener("click", function() {
+	p.textContent = "Le coude : la partie du membre supérieur située entre le bras et l'avant-bras. Cette articulation comprend en avant la région du « pli du coude » (ou fosse cubitale). C'est un complexe articulaire synovial du membre supérieur humain reliant le bras à l'avant-bras. Il unit ainsi trois os entre eux : le radius, l'ulna (cubitus) et l'humérus. Elle est la réunion de trois articulations : huméro-ulnaire, huméro-radiale et radio-ulnaire proximale (supérieure). Lorsque l'avant-bras est tendu (extension complète), le bras et l'avant-bras ne sont pas alignés dans le plan frontal.";
+});
+
+btn4.addEventListener("click", function() {
+	p.textContent = "Le genou : articulation qui permet de joindre la jambe à la cuisse. Elle met en jeu trois os, le fémur, le tibia et la patella, par le biais de trois articulations, l'articulation fémoro-patellaire et la double articulation fémoro-tibiale. Le cartilage assure la fluidité des mouvements du genou. Le tissu élastique fin, le cartilage, protège l'os et fait en sorte que les surfaces de l'articulation glissent facilement les unes contre les autres.";
+});
+
+btn5.addEventListener("click", function() {
+	p.textContent = "La cheville ou cou-de-pied : l'articulation qui relie la jambe et le pied. Elle est parfois sujette à des entorses, le plus souvent externes par flexion plantaire et pied en équin. Une cheville adulte est composée, au point de vue osseux, de l'épiphyse inférieure du tibia (malléole interne et plafond), de l'épiphyse inférieure de la fibula (ou péroné) (malléole externe) et du talus (ou astragale). Classiquement, on parle d'articulation supinale de l'arrière pied.";
+});
+
+btn6.addEventListener("click", function() {
+	p.textContent = "La hanche ou articulation coxo-fémorale : est une articulation (énarthrose) qui permet de joindre la cuisse au bassin. Elle met en jeu deux os : l'os iliaque et le fémur.";
+});
+
+
+
